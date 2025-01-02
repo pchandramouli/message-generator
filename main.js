aa9f2d42771416b4a8b4a610c850b23d8b115b38
Chandramouli P <contactmouli@gmail.com>
2025-01-02 20:38:45 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const messages = [
+    "Arise! Awake! And stop not till the goal is reached.  - Swami Vivekananda",
+    "A human being should be able to change a diaper, plan an invasion, butcher a hog, conn a ship, design a building, write a sonnet, balance accounts, build a wall, set a bone, comfort the dying, take orders, give orders, cooperate, act alone, solve equations, analyze a new problem, pitch manure, program a computer, cook a tasty meal, fight efficiently, die gallantly. Specialization is for insects.  - Robert A. Heinlein",
+    "Genius is one percent inspiration and ninety-nine percent perspiration.  - Thomas Alva Edison",
+    "Rest if you must, but don't you quit.  - Edgar A. Guest",
+    "There is neither this world nor the world beyond nor happiness for the one who doubts.  - Sri Krishna",
+    "Agalaadhu anugaadhu theekkaaivaar polvar igalvendhar serndhozhuguvaar.  -Thiruvalluvar"
+]
+console.log('Here\'s a random message for you:');
+console.log(messages[Math.floor(Math.random() * 6)]);
